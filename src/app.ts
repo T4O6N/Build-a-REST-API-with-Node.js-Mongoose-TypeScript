9756006e25ee881a3247552c631cc209dcd4a5ae
va1ent0n46 <3t9o9n9@gmail.com>
2024-03-27 16:19:47 +0700

    create utils for environment

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello world')
+import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+
+const port = config.get<number>("port");
+
+const app = express();
+
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+
+  await connect();
+});
